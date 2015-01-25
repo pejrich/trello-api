adc5410b355f2c70a992deae32f8c3d0d2f552cb
Peter Richards <peter.jrichards@yahoo.com>
2015-01-26 00:36:05 +0900

    Seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Project.destroy_all
+
+project = Project.new
+project.token = 1
+project.name = "First Project"
+project.save
+
+bucket = project.buckets.create
+bucket.name = "Test Bucket 1"
+bucket.save
+task = bucket.tasks.create
+task.name = "Test Task 1-1"
+task.save
+task = bucket.tasks.create
+task.name = "Test Task 1-2"
+task.save
+
+bucket = project.buckets.create
+bucket.name = "Test Bucket 2"
+bucket.save
+task = bucket.tasks.create
+task.name = "Test Task 2-1"
+task.save
+task = bucket.tasks.create
+task.name = "Test Task 2-2"
+task.save
